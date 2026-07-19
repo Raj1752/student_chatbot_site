b3d0c0533917eca2d623343c0713a06b991eeb9f
Raj1752 <abhisek.jugmohun@yahoo.com>
2026-07-19 15:47:10 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from rag_engine import get_rag_response as get_response
+import time
 
 app = Flask(__name__)
 
@@ -11,8 +12,17 @@ def home():
 def ask():
     data = request.get_json()
     user_question = data.get("question", "")
+
+    start = time.perf_counter()
+
     reply = get_response(user_question)
-    return jsonify({"answer": reply})
+
+    response_time = round(time.perf_counter() - start, 3)
+
+    return jsonify({
+        "answer": reply,
+        "time": response_time
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
